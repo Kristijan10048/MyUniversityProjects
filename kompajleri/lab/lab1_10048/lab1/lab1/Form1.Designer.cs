a4a82aa11203177fc803a25e71721566a7fa7ebb
Kristijan10048 <kristijan.petkov@gmail.com>
2020-01-04 13:50:10 +0100

    Kompajleri files

---
+++ kompajleri/lab/lab1_10048/lab1/lab1/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace lab1
+{
+    partial class form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.izrazTb = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 52);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Провери";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // izrazTb
+            // 
+            this.izrazTb.Location = new System.Drawing.Point(106, 55);
+            this.izrazTb.Name = "izrazTb";
+            this.izrazTb.Size = new System.Drawing.Size(343, 20);
+            this.izrazTb.TabIndex = 3;
+            // 
+            // output
+            // 
+            this.output.Location = new System.Drawing.Point(12, 96);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.output.Size = new System.Drawing.Size(437, 322);
+            this.output.TabIndex = 4;
+            // 
+            // form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(464, 428);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.izrazTb);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.Name = "form1";
+            this.Text = "лаб1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox izrazTb;
+        private System.Windows.Forms.TextBox output;
+    }
+}
+
---
+++ kompajleri/lab/lab1_10048/lab1/lab1/Form1.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace lab1
+{
+    public partial class form1 : Form
+    {
+        private string izraz;
+        byte p1p0 = 1;
+        byte p1n0 = 0;
+        byte n1p0 = 0;
+        byte n1n0 = 0;
+
+        public void initState()
+        {
+             p1p0 = 1;
+             p1n0 = 0;
+             n1p0 = 0;
+             n1n0 = 0;
+        }
+        private void printState(char ch)
+        {
+           
+            output.Text += "------------------------" + "\r\n";
+            output.Text += "p1p0=" + p1p0 + "\r\n";
+            output.Text += "p1n0=" + p1n0 + "\r\n";
+            output.Text += "n1p0=" + n1p0 + "\r\n";
+            output.Text += "n1n0=" + n1n0 + "\r\n";
+           
+        }
+
+        private void changeState(char ch)
+        {
+
+            //sostojba p1p0 i 1 na vlez
+            //premin vo n1p0
+            //pocetna i krajna sostojba
+            if ((ch == '1') && (p1p0 == 1))
+            {
+                p1p0 = 0;
+                p1n0 = 0;
+                n1p0 = 1;
+                n1n0 = 0;
+                printState(ch);
+                return;
+            }
+
+            //sostojba p1p0 i 0 na vlez
+            //premin vo p1n0
+            if ((ch == '0') && (p1p0 == 1))
+            {
+                p1p0 = 0;
+                p1n0 = 1;
+                n1p0 = 0;
+                n1n0 = 0;
+                printState(ch);
+                return;
+            }
+
+
+            //sostojba p1n0 i 0 na vlez
+            //premin vo p1p0
+            if ((ch == '0') && (p1n0 == 1))
+            {
+                p1p0 = 1;
+                p1n0 = 0;
+                n1p0 = 0;
+                n1n0 = 0;
+                printState(ch);
+                return;
+            }
+
+
+            //sostojba n1p0 i 1 na vlez
+            //premin vo p1p0
+            if ((ch == '1') && (n1p0 == 1))
+            {
+                p1p0 = 1;
+                p1n0 = 0;
+                n1p0 = 0;
+                n1n0 = 0;
+                printState(ch);
+                return;
+            }
+
+
+            //sostojba n1p0 i 0 na vlez
+            //premin vo n1n0
+            if ((ch == '0') && (n1p0 == 1))
+            {
+                p1p0 = 0;
+                p1n0 = 0;
+                n1p0 = 0;
+                n1n0 = 1;
+                printState(ch);
+                return;
+            }
+
+
+            //sostojba n1n0 i 0 na vlez
+            //premin n1p0
+            if ((ch == '0') && (n1n0 == 1))
+            {
+                p1p0 = 0;
+                p1n0 = 0;
+                n1p0 = 1;
+                n1n0 = 0;
+                printState(ch);
+                return;
+            }
+
+            //sostojba n1n0 i 1 na vlez
+            //premin p1n0
+            if ((ch == '1') && (n1n0 == 1))
+            {
+                p1p0 = 0;
+                p1n0 = 0;
+                n1p0 = 1;
+                n1n0 = 0;
+                printState(ch);
+                return;
+            }
+
+            //sostojba p1n0 i 1 na vlez
+            //premin n1n0
+            if ((ch == '1') && (p1n0 == 1))
+            {
+                p1p0 = 0;
+                p1n0 = 0;
+                n1p0 = 0;
+                n1n0 = 1;
+                printState(ch);
+                return;
+            }
+
+
+        }
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (izrazTb.Text != null)
+            {
+                output.Text = "";
+                izraz=izrazTb.Text;
+                initState();
+                int i;
+                char ch1;
+                for (i = 0; i < izraz.Length; i++)
+                {
+                    ch1=izraz[i];
+                    if ((ch1 != '0') && (ch1 != '1'))
+                        output.Text = "Внесениот израз не се состои од азбуката {0,1}";
+                    else
+                    {
+                        changeState(ch1);
+                        // output.Text+= "t"+"\r\n";
+                    }
+
+                }
+                if(p1p0==1)
+                    output.Text += "Внесениот израз e валиден";
+                else
+                    output.Text += "Внесениот израз не e валиден";
+            }
+        }
+    }
+}
